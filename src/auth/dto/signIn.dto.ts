1cbced8d5c9c28bf5698ba9688803d7de2720007
Clara Charriau <charriau.clara@gmail.com>
2024-04-09 12:51:08 +0200

    feat: add POST /signin endpoint

--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -1,4 +1,5 @@
 export interface SignInDto {
     email: string;
+    pseudo: string;
     password: string;
 }
--- test/service/user.service.spec.ts
+++ test/service/user.service.spec.ts
@@ -17,6 +17,10 @@ describe('UserService', () => {
         {
           provide: getRepositoryToken(User),
           useClass: Repository,
+          useValue: {
+            existsBy: jest.fn(),
+            save: jest.fn(),
+          },
         },
       ],
     }).compile();
@@ -31,7 +35,7 @@ describe('UserService', () => {
   it('should return a user when user with given email exists', async () => {
     const email = 'email@mail.com';
     const user = {
-      id: '1',
+      id: "b7d10dc9-af37-4eb1-b67d-5fe347af5682",
       email,
       pseudo: 'monicaGeller',
       password: '$2b$10$Xh.fhtFvRA12F2qMjbSbEe9YVZHLhNBW3CIOtKqEiOJbkldYO2L5q',
@@ -51,5 +55,41 @@ describe('UserService', () => {
     await expect(service.findUserByEmail(email)).rejects.toThrow(
       NotFoundException,
     );
+  });
+
+  it('should return true if user exists', async () => {
+    const email = 'test@example.com';
+    jest.spyOn(userRepository, 'existsBy').mockResolvedValue(true);
+
+    const result = await service.existsByEmail(email);
+
+    expect(result).toBe(true);
+  });
+
+  it('should return false if user does not exist', async () => {
+    const email = 'test@example.com';
+    jest.spyOn(userRepository, 'existsBy').mockResolvedValue(false);
+
+    const result = await service.existsByEmail(email);
+
+    expect(result).toBe(false);
+  });
+
+  it('should create a new user', async () => {
+    const email = 'test@example.com';
+    const pseudo = 'testUser';
+    const password = 'testPassword';
+    const newUser = {
+      id: expect.any(String),
+      email,
+      pseudo,
+      password: expect.any(String),
+    };
+
+    jest.spyOn(userRepository, 'save').mockResolvedValue(newUser);
+
+    const result = await service.createUser(email, pseudo, password);
+
+    expect(result).toEqual(newUser);
   });
 });
