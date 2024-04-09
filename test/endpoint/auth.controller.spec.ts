dd2ea2f1126e18bc2e8ff4a4ad7524a4834411a9
Clara Charriau <charriau.clara@gmail.com>
2024-04-09 11:07:45 +0200

    chore: set up test coverage

--- test/endpoint/auth.controller.spec.ts
+++ test/endpoint/auth.controller.spec.ts
@@ -1,10 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from '../../src/auth/auth.controller';
 import { AuthService } from '../../src/auth/auth.service';
+import { VerifyResponseDto } from 'src/auth/dto/verifyResponse.dto';
 
 describe('AuthController tests', () => {
   let controller: AuthController;
   const mockToken = { access_token: 'mockToken' };
+  const mockVerifyResponse: VerifyResponseDto = {
+    isExistingAccount: true,
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,8 +17,12 @@ describe('AuthController tests', () => {
         {
           provide: AuthService,
           useValue: {
-            signIn: jest.fn()
-            .mockImplementation(() => Promise.resolve(mockToken))
+            signIn: jest
+              .fn()
+              .mockImplementation(() => Promise.resolve(mockToken)),
+            verify: jest
+              .fn()
+              .mockImplementation(() => Promise.resolve(mockVerifyResponse)),
           },
         },
       ],
@@ -27,7 +35,7 @@ describe('AuthController tests', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should return a JWT token when user signs in', async () => {
+  it('should return a JWT token when user signs in successfully', async () => {
     // Given
     const email = 'test@example.com';
     const password = 'password';
@@ -38,5 +46,19 @@ describe('AuthController tests', () => {
 
     // Then
     expect(result).toEqual(mockToken);
+  });
+
+  it('should return true if user exists', async () => {
+    // Given
+    const email = 'test@example.com';
+    const mockVerifyResponse: VerifyResponseDto = {
+      isExistingAccount: true,
+    };
+
+    // When
+    const result = await controller.verify({ email });
+
+    // Then
+    expect(result).toEqual(mockVerifyResponse);
   });
 });
